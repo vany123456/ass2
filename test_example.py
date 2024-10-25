5255060cb4b0c81874ab3f54078459302e55d599
vany123456 <163380676+vany123456@users.noreply.github.com>
2024-10-26 00:16:47 +1030

    Create test_example.py

---
+++ test_example.py
@@ -0,0 +1,12 @@
+# test_example.py
+import unittest
+from example import add
+
+class TestAddFunction(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(0, 0), 0)
+
+if __name__ == '__main__':
+    unittest.main()
